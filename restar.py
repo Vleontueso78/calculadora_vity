e21f4ab02b61002c90d4754d395dfc8b536c861f
Vleontueso78 <vleontueso78@gmail.com>
2025-04-19 14:33:16 -0300

    Resta la cantidad de números que se agreguen

--- restar.py
+++ restar.py
@@ -1,4 +1,9 @@
-# Función para restar...
+def restar_lista(numeros):
+    if not numeros:
+        return 0  # o podrías lanzar un error, según lo que necesites
 
-def restar(a, b):
-    return a - b
+    resultado = numeros[0]
+    for num in numeros[1:]:
+        resultado -= num
+
+    return resultado
